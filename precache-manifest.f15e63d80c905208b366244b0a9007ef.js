a1b3694699a8de0285696ce4bc65ab9e4bc86d77
rishabhkailey <rishabhveersinghkailay@gmail.com>
2020-02-18 19:20:53 +0530

    Updates

---
+++ precache-manifest.f15e63d80c905208b366244b0a9007ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908940de629eb6a08df4e8adc3ea30b3",
+    "url": "/chess-deploy.git/index.html"
+  },
+  {
+    "revision": "5e7a2881127a83b6baa3",
+    "url": "/chess-deploy.git/static/css/main.4d41f9a2.chunk.css"
+  },
+  {
+    "revision": "cb8681019a96526b8a7d",
+    "url": "/chess-deploy.git/static/js/2.b1fda4e3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/chess-deploy.git/static/js/2.b1fda4e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e7a2881127a83b6baa3",
+    "url": "/chess-deploy.git/static/js/main.68ce6ea9.chunk.js"
+  },
+  {
+    "revision": "d3895a32bb6f21058cfc",
+    "url": "/chess-deploy.git/static/js/runtime-main.70cf63c6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rishabhkailey/chess-deploy.git/precache-manifest.825684d9d114c8ac75834a17204b5da0.js"
+  "/chess-deploy.git/precache-manifest.f15e63d80c905208b366244b0a9007ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rishabhkailey/chess-deploy.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chess-deploy.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
